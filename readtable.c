045135fadb9eade88a615ab3cb19837dd6739f70
rena2henriques <renato.manao@gmail.com>
2017-09-28 16:59:01 +0100

    read and print the table just for setting up the fscanf and fgets functions

--- readtable.c
+++ readtable.c
@@ -3,6 +3,11 @@
 void readTable(int argc, char const *argv[]) {
 	
 	FILE *file = NULL;
+	char temp[40];
+
+	/* temporary variables */
+	char prefix[32];
+	int nexthop = 0;
 
 	/* opening the file, if the user gives a file on command line, it's used, 
 	otherwise it uses the PrefixTable.txt file*/
@@ -18,7 +23,23 @@ void readTable(int argc, char const *argv[]) {
 		}
 	}
 
-	/* Do Magic stuff */
+	/* Temporary reading of table, in the future it must create a tree */
 
+	while (!feof(file)) {
+		fgets(temp, sizeof(temp), file);
+		sscanf(temp, "%s %d", prefix, &nexthop);
+		printf("%s %d\n", prefix, nexthop);
+
+		memset(prefix, 0, sizeof(prefix));
+
+	}
+
+	/* */
+
+
+	if ( fclose(file) == EOF ) {
+			printf("Error while closing file: %s\n", strerror(errno));
+			return;
+		}
 }
 
