c6043eefcc991f6662c7e438a34ba31af8927835
rena2henriques <renato.manao@gmail.com>
2017-10-09 15:49:44 +0100

    now it's prettier than ever

--- main.c
+++ main.c
@@ -25,17 +25,18 @@ int main(int argc, char const *argv[])
 	struct Node *root = PrefixTree(argc, argv);
 	struct TwoBitNode *root_two = NULL;	
 
+	printf("\n");
+	printf("What do you want to do:\n");
+	printf(" - write 1 if you want to print the table;\n");
+	printf(" - write 2 and an Adress if you want to look up the next-hop of said address;\n");
+	printf(" - write 3, a prefix and the associated next-hop to write them in the table;\n");
+	printf(" - write 4 and a prefix if you want to remove the prefix from the table;\n");
+	printf(" - write 5 if you want to print an even prefix table;\n");
+	printf(" - write exit if you want to exit the program\n");
+
 	while(1) {
-		printf("\n");
-		printf("What do you want to do:\n");
-		printf(" - write 1 if you want to print the table;\n");
-		printf(" - write 2 and an Adress if you want to look up the next-hop of said address;\n");
-		printf(" - write 3, a prefix and the associated next-hop to write them in the table;\n");
-		printf(" - write 4 and a prefix if you want to remove the prefix from the table;\n");
-		printf(" - write 5 if you want to print an even prefix table;\n");
-		printf(" - write exit if you want to exit the program\n");
+		printf("\n >>");
 		fgets(temp, sizeof(temp), stdin);
-		printf("\n");
 		
 		if( sscanf(temp, "%d", &user_choice) == 1 && user_choice == 1) {
 			printf("Prefix Table:\n");
@@ -89,12 +90,13 @@ int main(int argc, char const *argv[])
 				break;
 			}
 		}
-				
+		
 		//clear buffers
 		memset(address, 0, sizeof(address));
 		memset(temp, 0, sizeof(temp));
 		memset(prefix_input, 0, sizeof(prefix_input));
 		memset(aux, 0, sizeof(aux));
+
 	}
 
 	return 0;
